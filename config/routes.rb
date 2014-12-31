2f4a7a6caed389150e629b5d705edf14bf3ae5ff
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-01-01 01:12:57 +0900

    change routing
    http://stackoverflow.com/questions/16700801/ruby-on-rails-heroku-the-page-you-were-looking-for-doesnt-exist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FirstApp::Application.routes.draw do
-  root  'static_pages#home'
+  root to: 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
